3da0f764e2f320b422846969ae815d3a8085e80f
ariafatah <ariafatah0711@gmail.com>
2023-09-10 20:56:43 +0700

    update

--- script/script.js
+++ script/script.js
@@ -67,29 +67,3 @@ window.onload = clearTextarea;
 function refresh() {
   location.reload(); // Akan merefresh halaman
 }
-
-/*
-// mengatur tampilan android
-let currentZoomLevel = 1; // Inisialisasi level zoom awal
-let isAndroid = /Android/.test(navigator.userAgent);
-
-// Fungsi untuk mendeteksi perubahan zoom
-function detectZoomChange() {
-  let zoomLevel = document.documentElement.clientWidth / window.innerWidth;
-
-  // Cek apakah perangkat Android dan level zoom berubah
-  if (isAndroid && zoomLevel !== currentZoomLevel) {
-    // Sesuaikan posisi halaman di sini (contohnya, mengatur margin-top)
-    document.body.style.marginTop = "20px"; // Sesuaikan dengan nilai yang Anda inginkan
-
-    // Update nilai zoom saat ini
-    currentZoomLevel = zoomLevel;
-  }
-
-  // Panggil fungsi ini lagi secara berulang
-  requestAnimationFrame(detectZoomChange);
-}
-
-// Panggil fungsi pertama kali
-detectZoomChange();
-*/
