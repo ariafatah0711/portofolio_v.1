9e5f4cb2df28972cd9efdda874260290b9c915cc
ariafatah <ariafatah0711@gmail.com>
2023-09-11 17:54:36 +0700

    update

--- script/script.js
+++ script/script.js
@@ -93,9 +93,13 @@ function showScrollBtn() {
   scrollBtn.style.display = "block";
 
   setTimeout(() => {
+    // scrollBtn.style.bottom = "100px";
     scrollBtn.style.display = "none";
   }, 3000);
+  // scrollBtn.style.bottom = "25px";
 }
 
-window.addEventListener("scroll", showScrollBtn);
-window.addEventListener("mousemove", showScrollBtn);
+// window.addEventListener("scroll", showScrollBtn);
+// window.addEventListener("mousemove", showScrollBtn);
+// window.addEventListener("touchmove", showScrollBtn);
+// window.addEventListener("touch", showScrollBtn);
